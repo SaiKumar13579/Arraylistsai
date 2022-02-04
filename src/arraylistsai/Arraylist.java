f707a9447781ba49ae95ee5d3fdde9fdae8e91d9
SaiKumar13579 <89684405+SaiKumar13579@users.noreply.github.com>
2022-02-05 07:48:41 +1100

    Arraylist's

---
+++ src/arraylistsai/Arraylist.java
@@ -0,0 +1,50 @@
+package arraylistsai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        List<String> pets = new ArrayList<>();
+
+        System.out.println(true);
+        System.out.println(0);
+
+        if(pets.isEmpty()) {
+            System.out.println("no pets");
+        }
+
+        if(pets.size() == 0) {
+            System.out.println("no pets");
+        }
+
+        pets.add("cat");
+        System.out.println(pets.isEmpty());
+        System.out.println(pets.size());
+
+        pets.clear();
+        System.out.println(pets.isEmpty());
+        System.out.println(pets.size());
+
+        pets.add("dog");
+        System.out.println(pets.contains("cat")); // false
+        System.out.println(pets.contains("dog")); // true
+
+        List<String> newPets = new ArrayList<>();
+        newPets.add("dog");
+
+        System.out.println(pets.equals(newPets));
+
+        newPets.add("cat");
+        System.out.println(pets.equals(newPets));
+
+        pets.add(0, "cat");
+        System.out.println(pets);
+        System.out.println(newPets);
+
+        // size and elements are equal but order of elements is not equal
+        System.out.println(pets.equals(newPets));
+    }
+}
+
+
